7111d232e14694cbad2e22be42a8843990d6639f
Rashmy Shiraj <rashmyshiraj1@gmail.com>
2026-01-17 22:44:25 +0530

    connected the database

---
+++ src/main/java/com/rashmy/library/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.rashmy.library.controller;
+
+import com.rashmy.library.entity.Book;
+import com.rashmy.library.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+}
