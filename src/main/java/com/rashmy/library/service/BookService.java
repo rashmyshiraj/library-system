a467ed70f9065ad76f74ebcb961fe3e73b29309a
Rashmy Shiraj <rashmyshiraj1@gmail.com>
2026-01-17 21:58:43 +0530

    created and implemented the book service

---
+++ src/main/java/com/rashmy/library/service/BookService.java
@@ -0,0 +1,25 @@
+package com.rashmy.library.service;
+
+import com.rashmy.library.entity.Book;
+import com.rashmy.library.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
